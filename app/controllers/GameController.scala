a5a99d00eb412038ec152099819c9f4576c9c5c1
simon <simon@keppgrund5.de>
2020-11-11 17:27:23 +0100

    connection back frontend

--- app/controllers/GameController.scala
+++ app/controllers/GameController.scala
@@ -3,7 +3,7 @@ package controllers
 import aview.TUI.{TUI, TUIInterface}
 import gamecontrol.supervisor.{SupervisorInterface, supervisor}
 import gamecontrol.controller.{Controller, ControllerInterface}
-import gamecontrol.updateEvent
+import gamecontrol.{InsertedEvent, updateEvent}
 import javax.inject.Inject
 import play.api.mvc.{AbstractController, Action, AnyContent, ControllerComponents, WebSocket}
 import play.api.i18n.I18nSupport
@@ -17,39 +17,40 @@ class GameController @Inject() (cc:ControllerComponents) extends AbstractControl
   val supervisor: SupervisorInterface = scala.main.supervisor
   val controller: ControllerInterface = scala.main.Controller
   supervisor.controller = controller
-  this.listenTo(supervisor.controller)
+  var str:String = ""
 
+  this.listenTo(controller)
   reactions += {
-    case event : updateEvent=>
-    if(event.code == 0)
-      sendline(event.word)
-    if(event.code == 1)
-      send(event.word)
+    case event: InsertedEvent =>
+        this.send(supervisor)
+
   }
-  def sendline(s: String) = Action {
-    Ok(views.html.squarecastle(1,"empfangen",s ,supervisor.playersturn))
-  }
-  def send(s: String) = Action {
-    Ok(views.html.squarecastle(0,"empfangen",s ,supervisor.playersturn))
-  }
+
   def put(s: String): Action[AnyContent] = Action {
+
     controller.befehl = s
     //supervisor.controller.befehl = s;
     supervisor.newRoundactive()
     supervisor.state = !supervisor.state
     supervisor.newRound()
 
-    Ok(views.html.squarecastle(1,"gesendet","",supervisor.playersturn))
+    Ok(views.html.squarecastle("gesendet",supervisor))
 
   }
   def squarecastle: Action[AnyContent] = Action{
     supervisor.testfall();
     supervisor.newRound()
 
-    Ok(views.html.squarecastle(1,"STARTE SPIEL","",supervisor.playersturn))
+    Ok(views.html.squarecastle("STARTE SPIEL",supervisor))
   }
 
   def about(): Action[AnyContent] = Action {
     Ok(views.html.index())
+  }
+
+  def send(supervisor:SupervisorInterface): Unit = {
+    Ok(views.html.squarecastle("empfangen",supervisor))
+    //views.html.squarecastle.apply("empfangen", s , supervisor.playersturn)
+    //views.html.squarecastle.render("empfangen", s , supervisor.playersturn)
   }
 }
